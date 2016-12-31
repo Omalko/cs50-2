5ad1c872e482e098323567329420312d62a17a7d
Wes Basinger <dbumathlete@gmail.com>
2016-12-31 19:40:58 +0000

    Trying to figure out a float error.

---
+++ pset1/greedy.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int GetCorrectFloat();
+int convertFloatToInt();
+
+int main(void) 
+{
+
+	int changeFloat = GetCorrectFloat();
+	printf("%d", changeFloat);
+	printf("\n");
+
+}
+
+int GetCorrectFloat(void) 
+{
+
+	float change;
+	do
+	{
+		printf("How much change is owed?\n");
+		change = GetFloat();
+	}
+	while (change < 0);
+
+	int totalCoins = 0;
+	
+	int cents = change * 100;
+	// how many quarters?
+	int numQuarters = cents / 25;
+	printf("I calculate that you need %d quarters.\n", numQuarters);
+	totalCoins += numQuarters;
+	int remainingAfterQuarters = cents - (numQuarters*25);
+	printf("There are %d cents remaining after quarters.\n", remainingAfterQuarters);
+	//how many dimes?
+	int numDimes = remainingAfterQuarters / 10;
+	totalCoins += numDimes;
+	int remainingAfterDimes = remainingAfterQuarters - (numDimes*10);
+	//how many nickels?
+	int numNickels = remainingAfterDimes / 5;
+	totalCoins += numNickels;
+	int numPennies = remainingAfterDimes - (numNickels*5);
+	totalCoins += numPennies;
+	return totalCoins;
+	
+}
+
