2a61665b3c74a501af8ed49be8f95024ac1f336b
Wes Basinger <dbumathlete@gmail.com>
2017-01-01 23:33:31 +0000

    Got lots of nice print hinting.

---
+++ pset2/vigenere.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+int main(int argc, string argv[1])
+{
+    if (argc != 2)
+    {
+        printf("Usage: ./vigenere str\n");
+        return 1;
+    }
+    
+    string pword = argv[1];
+    
+    int pword_len = strlen(pword);
+    
+    printf("plaintext: ");
+    
+    string ptext = get_string();
+    
+    int curr_pos = 0;
+    int curr_pword_pos = 0;
+    
+    while (curr_pos < strlen(ptext))
+    {
+        int ascii = (int) ptext[curr_pos];
+        // checks if curr_pos is a letter
+        if ((ascii > 64 && ascii < 91) || (ascii > 96 && ascii < 123))
+        {
+            int ptext_ascii = (int) ptext[curr_pos];
+            int pword_ascii = (int) pword[curr_pword_pos % pword_len];
+            curr_pword_pos ++;
+            
+            printf("ptext_ascii is: %d\n", ptext_ascii);
+            printf("pword_ascii is: %d\n", pword_ascii);
+            printf("reg letter is: %c\n", ptext[curr_pos]);
+            
+        }
+        else
+        {
+            printf("%c\n", ptext[curr_pos]);
+        }
+        curr_pos ++;
+    }
+    
+    /**
+    for (int i=0; i<strlen(ptext); i++)
+    {
+        int ptext_ascii = (int) ptext[i];
+        if ((ascii > 64 && ascii < 91) || (ascii > 96 && ascii < 123))
+        {
+            
+        }
+        int pword_ascii = (int) pword[i % pword_len];
+        
+        printf("ptext_ascii is: %d\n", ptext_ascii);
+        printf("pword_ascii is: %d\n", pword_ascii);
+        
+    }
+    **/
+    
+    printf("%s\n", argv[1]);
+    printf("%s\n", pword);
+}
