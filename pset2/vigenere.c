82a2df6842011bdac5d71bfbf8131ccc906bbc82
Wes Basinger <dbumathlete@gmail.com>
2017-01-02 02:01:21 +0000

    Working great, just need to figure out how to reject an int arg.

--- pset2/vigenere.c
+++ pset2/vigenere.c
@@ -19,6 +19,8 @@ int main(int argc, string argv[1])
     
     string ptext = get_string();
     
+    printf("ciphertext: ");
+    
     int curr_pos = 0;
     int curr_pword_pos = 0;
     
@@ -28,69 +30,49 @@ int main(int argc, string argv[1])
         // checks if curr_pos is a letter
         if ((ascii > 64 && ascii < 91) || (ascii > 96 && ascii < 123))
         {
-            int ptext_ascii = (int) ptext[curr_pos];
             int pword_ascii = (int) pword[curr_pword_pos % pword_len];
             curr_pword_pos ++;
-            
-            printf("ptext_ascii is: %d\n", ptext_ascii);
-            printf("pword_ascii is: %d\n", pword_ascii);
-            printf("reg letter is: %c\n", ptext[curr_pos]);
-            
-            int ptext_lower = tolower(ptext_ascii);
+
             int pword_lower = tolower(pword_ascii);
             
             int pword_reg_index = pword_lower - 97;
             
-            int shift = (int) pword_lower - (int) ptext_lower;
-            
-            printf("PWord_reg_index is: %d\n", pword_reg_index);
-            printf("Shift is %d\n", shift);
-            
-            if (ptext_ascii > 64 && ptext_ascii < 91)
+
+            //handle upper case
+            if (ascii > 64 && ascii < 91)
             {
-                int result = ptext_ascii + pword_reg_index;
+                int result = ascii + pword_reg_index;
                 if (result <= 90)
                 {
-                    printf("###########\n");
                     printf("%c", result);
-                    printf("###########\n");
+                }
+                else
+                {
+                    int zshift = result - 90;
+                    printf("%c", zshift + 64);
                 }
             }
-            else if (ptext_ascii > 96 && ptext_ascii < 123)
+            //handle lower case
+            else if (ascii > 96 && ascii < 123)
             {
-                int result = ptext_ascii + pword_reg_index;
+                int result = ascii + pword_reg_index;
                 if (result <= 122)
                 {
-                    printf("###########\n");
                     printf("%c", result);
-                    printf("###########\n");
+                } else
+                {
+                    int zshift = result - 122;
+                    printf("%c", zshift + 96);
                 }
             }
             
         }
         else
         {
-            printf("%c\n", ptext[curr_pos]);
+            printf("%c", ptext[curr_pos]);
         }
         curr_pos ++;
     }
-    
-    /**
-    for (int i=0; i<strlen(ptext); i++)
-    {
-        int ptext_ascii = (int) ptext[i];
-        if ((ascii > 64 && ascii < 91) || (ascii > 96 && ascii < 123))
-        {
-            
-        }
-        int pword_ascii = (int) pword[i % pword_len];
-        
-        printf("ptext_ascii is: %d\n", ptext_ascii);
-        printf("pword_ascii is: %d\n", pword_ascii);
-        
-    }
-    **/
-    
-    printf("%s\n", argv[1]);
-    printf("%s\n", pword);
+    printf("\n");
+    return 0;
 }
