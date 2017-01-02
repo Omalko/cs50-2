efc2803799806e6a23d48e9005487aa3be0307ab
Wes Basinger <dbumathlete@gmail.com>
2017-01-02 19:12:32 +0000

    Using simple linear search.

--- pset3/find/helpers.c
+++ pset3/find/helpers.c
@@ -13,7 +13,13 @@
  */
 bool search(int value, int values[], int n)
 {
-    // TODO: implement a searching algorithm
+    for (int i=0; i<n; i++)
+    {
+        if (values[i] == value)
+        {
+            return true;
+        }
+    }
     return false;
 }
 
