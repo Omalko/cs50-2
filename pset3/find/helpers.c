888f2a09dc82d0e9f8cd940216dfd6f76f407ec7
Wes Basinger <dbumathlete@gmail.com>
2017-01-03 01:20:49 +0000

    Working passing all cs50 tests.

--- pset3/find/helpers.c
+++ pset3/find/helpers.c
@@ -15,16 +15,8 @@ bool search(int value, int values[], int n)
 {
     sort(values, n);
     
-    for (int k=0; k<n; k++)
-    {
-        printf("%d\n", values[k]);
-    }
-    
     int min = 0;
     int max = n - 1;
-    
-    printf("Initial min: %d\n", min);
-    printf("Initial max: %d\n", max);
     
     while (max - 1 > min)
     {
@@ -37,28 +29,20 @@ bool search(int value, int values[], int n)
             return true;
         }
         int guess = (max + min) / 2;
-        printf("Guess index is now: %d\n", guess);
         if (values[guess] == value)
         {
-            printf("FOUND IT!\n");
             return true;
         }
         else if (values[guess] < value)
         {
-            printf("Guess value %d was less than %d\n", values[guess], value);
             min = guess ++;
-            printf("New Min index: %d\n", min);
-            printf("Max stays at index %d\n", max);
+
         }
         else if (values[guess] > value)
         {
-            printf("Guess value %d was greater than %d\n", values[guess], value);
             max = guess --;
-            printf("New Max index: %d\n", max);
-            printf("Min stays at index %d\n", min);
         }
     }
-    printf("COULD NOT FIND IT!\n");
     return false;
 }
 
