837d8b248128bb1e25a7471749328beab3cdb9f5
Wes Basinger <wbasinger@villagetechschools.org>
2017-02-05 03:29:10 +0000

    Jacked smile up somehow and worked up analyzer  Seems to be working.

--- pset6/sentiments/analyzer.py
+++ pset6/sentiments/analyzer.py
@@ -6,10 +6,48 @@ class Analyzer():
     def __init__(self, positives, negatives):
         """Initialize Analyzer."""
 
-        # TODO
+        temp_positives = []
+        
+        f = open(positives)
+        
+        for line in f:
+            
+            if line[0] != ";":
+                
+                temp_positives.append(line.rstrip())
+        
+        f.close()
+        
+        temp_negatives = []
+        
+        f = open(negatives)
+        
+        for line in f:
+            
+            if line[0] != ";":
+                
+                temp_negatives.append(line.rstrip())
+                
+        f.close()
+        
+        self.positives = temp_positives
+        self.negatives = temp_negatives
 
     def analyze(self, text):
         """Analyze text for sentiment, returning its score."""
 
-        # TODO
-        return 0
+        tokens = nltk.word_tokenize(text)
+    
+        score = 0
+        
+        for token in tokens:
+            
+            if token in self.positives:
+                
+                score += 1
+                
+            elif token in self.negatives:
+                
+                score -= 1
+        
+        return score
