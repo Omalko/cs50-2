8cdf4db85a55e40f116d4ba18e9d5ba650b0c373
Wes Basinger <wbasinger@villagetechschools.org>
2017-03-15 19:09:11 +0000

    Changed center location to desoto, tx.

--- pset8/mashup/static/scripts.js
+++ pset8/mashup/static/scripts.js
@@ -37,7 +37,7 @@ $(function() {
     // options for map
     // https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     var options = {
-        center: {lat: 37.4236, lng: -122.1619}, // Stanford, California
+        center: {lat: 32.5897, lng: -96.8571}, // Desoto TX
         disableDefaultUI: true,
         mapTypeId: google.maps.MapTypeId.ROADMAP,
         maxZoom: 14,
